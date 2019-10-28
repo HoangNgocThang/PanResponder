987519d3f7d7984eda1d91c36e8433e9a182fcc1
HoangNgocThang <thanghoangngocutc@gmail.com>
2019-10-28 13:11:29 +0700

    cm

--- App.js
+++ App.js
@@ -1,44 +1,37 @@
-import React, { Fragment } from "react";
-import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import React, { Fragment, useState } from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  TextInput,
+  InputAccessoryView,
+  Button
+} from "react-native";
 import ExampleComponent from "./src/ExampleComponent";
 
 const App = () => {
+  const inputAccessoryViewID = "uniqueID";
+  const [text, setText] = useState("");
+
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ExampleComponent />
+        <TextInput
+          style={{
+            padding: 10,
+            paddingTop: 50
+          }}
+          inputAccessoryViewID={inputAccessoryViewID}
+          onChangeText={text => setText(text)}
+          value={text}
+        />
+        <InputAccessoryView nativeID={inputAccessoryViewID}>
+          <Button onPress={() => setText("aaaa")} title="Reset Text" />
+        </InputAccessoryView>
       </SafeAreaView>
     </Fragment>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter
-  },
-  body: {
-    backgroundColor: Colors.white
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-    color: Colors.black
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-    color: Colors.dark
-  },
-  highlight: {
-    fontWeight: "700"
-  }
-});
 
 export default App;
